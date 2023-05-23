939d313f3374ba93c6bb8743b75a2879735344cb
Amanda Simonds <amandasimonds9@gmail.com>
2023-05-23 15:34:56 -0400

    installed aurora pattern library wooooo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuroraButtonModule, CardModule } from 'ngx-aurora-pattern-library';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AuroraButtonModule,
+    CardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
