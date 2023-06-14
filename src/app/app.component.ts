c947d62e0e0e139ebecc55a05d3c3932f1f9323e
Amanda Simonds <amandasimonds9@gmail.com>
2023-06-14 12:11:16 -0400

    locally using lib, added dark mode toggle

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { Component, Inject, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'aurora-kit-demo-app';
+  public darkModeOn = false;
+
+  constructor(
+    @Inject(DOCUMENT) private document: Document,
+    private renderer: Renderer2
+  ) {}
+
+  public toggleDarkMode() {
+    if (this.darkModeOn) {
+      this.darkModeOn = false;
+      this.renderer.removeClass(this.document.body, 'aurora-dark-theme');
+    } else {
+      this.darkModeOn = true;
+      this.renderer.addClass(this.document.body, 'aurora-dark-theme');
+    }
+  }
 }
